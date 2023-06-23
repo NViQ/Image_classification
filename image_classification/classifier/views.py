1ed055fdcf315c4aa01b9b138b93643746b66d2d
Павел <naveq4358@gmail.com>
2023-06-24 02:35:51 +0500

    Commit

--- image_classification/classifier/views.py
+++ image_classification/classifier/views.py
@@ -6,11 +6,19 @@ from .utils import classify_image
 @csrf_exempt
 def upload_file(request):
     if request.method == 'POST':
-        file = request.FILES['file']
-        img = Image.open(file).convert('RGB')
-        width, height = img.size
-        class_name = classify_image(img)
-        return render(request, 'result.html', {'width': width, 'height': height, 'class_name': class_name})
+        if 'file' in request.FILES:
+            file = request.FILES['file']
+            if file:
+                img = Image.open(file).convert('RGB')
+                width, height = img.size
+                class_name = classify_image(img)
+                return render(request, 'result.html', {'width': width, 'height': height, 'class_name': class_name})
+            else:
+                error_message = "no file selected"
+                return render(request, 'upload.html', {'error_message': error_message})
+        else:
+            error_message = "no file selected"
+            return render(request, 'upload.html', {'error_message': error_message})
     return render(request, 'upload.html')
 
 def upload_another_image(request):
